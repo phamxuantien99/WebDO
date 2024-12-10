9318c3ba42cb791a44b19c97454df6a58e861cc1
Pham Xuan Tien <tienpham24399@gmail.com>
2024-12-11 01:30:56 +0700

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-
-//   theme: {
-//     extend: {},
-//   },
-
-//   plugins: [require("daisyui")],
-
-//   daisyui: {
-//     darkTheme: "light",
-//   },
-// };
-
-export default {
+module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
 
   theme: {
